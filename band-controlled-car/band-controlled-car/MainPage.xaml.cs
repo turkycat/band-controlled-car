d38862fd683db72e2439fab37c84b6421303b1d1
Jesse Frush <jesse.frush@gmail.com>
2015-11-05 00:01:49 -0800

    Added Band connection logic and skeleton for other functions

--- band-controlled-car/band-controlled-car/MainPage.xaml.cs
+++ band-controlled-car/band-controlled-car/MainPage.xaml.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Microsoft.Band;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -22,9 +24,49 @@ namespace band_controlled_car
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        IBandClient bandClient;
+
         public MainPage()
         {
             this.InitializeComponent();
         }
-    }
+
+        private async void Band_Connect( object sender, RoutedEventArgs e )
+        {
+            BandConnectionStatusText.Text = "Connecting ...";
+
+            try
+            {
+                // Get the list of Microsoft Bands paired to the phone.
+                IBandInfo[] pairedBands = await BandClientManager.Instance.GetBandsAsync();
+                if( pairedBands.Length < 1 )
+                {
+                    BandConnectionStatusText.Text = "This sample app requires a Microsoft Band paired to your device. Also make sure that you have the latest firmware installed on your Band, as provided by the latest Microsoft Health app.";
+                    return;
+                }
+
+                // Connect to Microsoft Band.
+                bandClient = await BandClientManager.Instance.ConnectAsync( pairedBands[0] );
+                
+                    // Subscribe to Accelerometer data.
+                bandClient.SensorManager.Accelerometer.ReadingChanged += Accelerometer_ReadingChanged;
+                await bandClient.SensorManager.Accelerometer.StartReadingsAsync();
+            }
+            catch( Exception ex )
+            {
+                BandConnectionStatusText.Text = ex.ToString();
+            }
+        }
+
+        private void Accelerometer_ReadingChanged( object sender, Microsoft.Band.Sensors.BandSensorReadingEventArgs<Microsoft.Band.Sensors.IBandAccelerometerReading> e )
+        {
+            BandConnectionStatusText.Text = String.Format( "{0}", e.SensorReading.AccelerationX );
+        }
+
+        private async void Arduino_Connect( object sender, RoutedEventArgs e )
+        {
+        }
+        }
+
+    
 }
