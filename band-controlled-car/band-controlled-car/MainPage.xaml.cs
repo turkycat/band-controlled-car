387bfec63e22235f7cfc96556e85acc1136daa61
Jesse Frush <jesse.frush@gmail.com>
2015-11-05 00:26:10 -0800

    Added driving logic (untested, unmodified) and moved necessary calls to the UI thread

--- band-controlled-car/band-controlled-car/MainPage.xaml.cs
+++ band-controlled-car/band-controlled-car/MainPage.xaml.cs
@@ -9,6 +9,7 @@ using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.System.Display;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -26,15 +27,49 @@ namespace band_controlled_car
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private const double LR_MAG = 0.4;
+        private const double FB_MAG = 0.4;
+        private const double MAX_ANALOG_VALUE = 255.0;
+        private const byte FB_DIRECTION_CONTROL_PIN = 8;
+        private const byte FB_MOTOR_CONTROL_PIN = 9;
+        private const byte LR_DIRECTION_CONTROL_PIN = 2;
+        private const byte LR_MOTOR_CONTROL_PIN = 3;
+
+        private const PinState LEFT = PinState.LOW;
+        private const PinState RIGHT = PinState.HIGH;
+        private const PinState FORWARD = PinState.LOW;
+        private const PinState REVERSE = PinState.HIGH;
+
+        private Turn turn;
+        private Direction direction;
+
         IBandClient bandClient;
 
         volatile bool arduinoConnected = false;
         IStream arduinoConnection;
         RemoteDevice arduino;
 
+        private enum Turn
+        {
+            none,
+            left,
+            right
+        }
+
+        private enum Direction
+        {
+            none,
+            forward,
+            reverse
+        }
+
+        private DisplayRequest keepScreenOnRequest;
+
         public MainPage()
         {
             this.InitializeComponent();
+            turn = Turn.none;
+            direction = Direction.none;
         }
 
         private async void Band_Connect( object sender, RoutedEventArgs e )
@@ -70,8 +105,16 @@ namespace band_controlled_car
         {
             if( arduinoConnected )
             {
-                //TODO
-            }
+                var action = Dispatcher.RunAsync( Windows.UI.Core.CoreDispatcherPriority.Normal, new Windows.UI.Core.DispatchedHandler( () =>
+                {
+                    //Y is the left/right tilt, while X is the fwd/rev tilt
+                    double lr = -e.SensorReading.AccelerationY;
+                    double fb = e.SensorReading.AccelerationX;
+
+                    handleTurn( lr );
+                    handleDirection( fb );
+            } ) );
+        }
         }
 
         private async void Arduino_Connect( object sender, RoutedEventArgs e )
@@ -89,19 +132,137 @@ namespace band_controlled_car
         private void Arduino_DeviceReady()
         {
             arduinoConnected = true;
-            ArduinoConnectionStatusText.Text = "Connected.";
+
+            var action = Dispatcher.RunAsync( Windows.UI.Core.CoreDispatcherPriority.Normal, new Windows.UI.Core.DispatchedHandler( () =>
+            {
+                ArduinoConnectionStatusText.Text = "Connected.";
+
+                arduino.pinMode( LR_DIRECTION_CONTROL_PIN, PinMode.OUTPUT );
+                arduino.pinMode( FB_DIRECTION_CONTROL_PIN, PinMode.OUTPUT );
+                arduino.pinMode( LR_MOTOR_CONTROL_PIN, PinMode.PWM );
+                arduino.pinMode( FB_MOTOR_CONTROL_PIN, PinMode.PWM );
+            } ) );
         }
 
         private void Arduino_DeviceConnectionFailed( string message )
         {
             arduinoConnected = false;
-            ArduinoConnectionStatusText.Text = "Connection Failed.";
+            arduinoConnection.end();
+            arduinoConnection = null;
+            arduino = null;
+
+
+            var action = Dispatcher.RunAsync( Windows.UI.Core.CoreDispatcherPriority.Normal, new Windows.UI.Core.DispatchedHandler( () =>
+            {
+                ArduinoConnectionStatusText.Text = "Connection Failed.";
+            } ) );
         }
 
         private void Arduino_DeviceConnectionLost( string message )
         {
             arduinoConnected = false;
-            ArduinoConnectionStatusText.Text = "Connection Lost.";
+            var action = Dispatcher.RunAsync( Windows.UI.Core.CoreDispatcherPriority.Normal, new Windows.UI.Core.DispatchedHandler( () =>
+            {
+                ArduinoConnectionStatusText.Text = "Connection Lost.";
+            } ) );
+        }
+
+        private void handleTurn( double lr )
+        {
+            //left and right turns work best using digital signals
+
+            if( lr < -LR_MAG )
+            {
+                //if we've switched directions, we need to be careful about how we switch
+                if( turn != Turn.left )
+                {
+                    //stop motor & set direction left
+                    arduino.digitalWrite( LR_MOTOR_CONTROL_PIN, PinState.LOW );
+                    arduino.digitalWrite( LR_DIRECTION_CONTROL_PIN, LEFT );
+                }
+
+                //start the motor by setting the pin high
+                arduino.digitalWrite( LR_MOTOR_CONTROL_PIN, PinState.HIGH );
+                turn = Turn.left;
+            }
+            else if( lr > LR_MAG )
+            {
+                if( turn != Turn.right )
+                {
+                    //stop motor & set direction right
+                    arduino.digitalWrite( LR_MOTOR_CONTROL_PIN, PinState.LOW );
+                    arduino.digitalWrite( LR_DIRECTION_CONTROL_PIN, RIGHT );
+                }
+
+                //start the motor by setting the pin high
+                arduino.digitalWrite( LR_MOTOR_CONTROL_PIN, PinState.HIGH );
+                turn = Turn.right;
+            }
+            else
+            {
+                //stop the motor
+                arduino.digitalWrite( LR_MOTOR_CONTROL_PIN, PinState.LOW );
+                turn = Turn.none;
+            }
+        }
+
+        private void handleDirection( double fb )
+        {
+            /*
+             * The neutral state is anywhere from (-0.5, 0), so that the phone can be held like a controller, at a moderate angle.
+             * This is because holding the phone at an angle is natural, tilting back to -1.0 is easy, while it feels awkward to tilt the phone
+             *  forward beyond 0.5 Therefore, reverse is from [-1.0, -0.5] and forward is from [0, 0.5].
+             *
+             * if the tilt goes beyond -0.5 in the negative direction the phone is being tilted backwards, and the car will start to reverse.
+             * if the tilt goes beyond 0 in the positive direction the phone is being tilted forwards, and the car will start to move forward.
+             */
+
+            if( fb < -FB_MAG )
+            {
+                //reading is less than the negative magnitude, the phone is being tilted back and the car should reverse
+                double weight = -( fb + FB_MAG );
+                byte analogVal = mapWeight( weight );
+
+                if( direction != Direction.reverse )
+                {
+                    //stop motor & set direction forward
+                    arduino.analogWrite( FB_MOTOR_CONTROL_PIN, 0 );
+                    arduino.digitalWrite( FB_DIRECTION_CONTROL_PIN, REVERSE );
+                }
+
+                //start the motor by setting the pin to the appropriate analog value
+                arduino.analogWrite( FB_MOTOR_CONTROL_PIN, analogVal );
+                direction = Direction.reverse;
+            }
+            else if( fb > 0 )
+            {
+                //reading is greater than zero, the phone is being tilted forward and the car should move forward
+                byte analogVal = mapWeight( fb );
+
+                if( direction != Direction.forward )
+                {
+                    //stop motor & set direction forward
+                    arduino.analogWrite( FB_MOTOR_CONTROL_PIN, 0 );
+                    arduino.digitalWrite( FB_DIRECTION_CONTROL_PIN, FORWARD );
+                }
+
+                //start the motor by setting the pin to the appropriate analog value
+                arduino.analogWrite( FB_MOTOR_CONTROL_PIN, analogVal );
+                direction = Direction.forward;
+            }
+            else
+            {
+                //reading is in the neutral zone (between -FB_MAG and 0) and the car should stop/idle
+                arduino.analogWrite( FB_MOTOR_CONTROL_PIN, 0 );
+                direction = Direction.none;
+            }
+        }
+
+        private byte mapWeight( double weight )
+        {
+            //the value should be [0, 0.5], but we want to clamp the value between [0, 1]
+            weight = Math.Max( Math.Min( weight * 2, 1.0 ), 0.0 );
+            return (byte)( weight * MAX_ANALOG_VALUE );
         }
     }
 }
